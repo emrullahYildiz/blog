bef142a72c2e2943670100d75e09d86e62a6650d
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-09 02:56:33 +0300

    Database context created

---
+++ Models/DatabaseContext.cs
@@ -0,0 +1,15 @@
+
+using Microsoft.EntityFrameworkCore;
+
+namespace blog.Models{
+    public class DatabaseContext:DbContext{
+        public DatabaseContext(DbContextOptions<DatabaseContext> options):base(options){
+
+        }
+        public DbSet<Article> Articles { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<About> About { get; set; }
+    }
+}
