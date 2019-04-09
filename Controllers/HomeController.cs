bff8edb624846f70a0b3a6b40b3b608f5c2ea07f
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-09 21:58:49 +0300

    category data passed

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,14 +5,23 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using blog.Models;
+using blog.Interfaces;
 
 namespace blog.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ICategory categoryContext;
+        public HomeController(ICategory categoryContext){
+            this.categoryContext = categoryContext;
+        }
         public IActionResult Index()
         {
-            return View();
+            HomeViewModel homeViewModel = new HomeViewModel{
+                categories = categoryContext.getCategories()
+            };
+            
+            return View(homeViewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
