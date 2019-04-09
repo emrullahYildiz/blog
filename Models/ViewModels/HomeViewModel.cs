ea9e9ec50b36e54e1ca15db1def89f2f39e78125
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-09 23:28:05 +0300

    article list added

--- Models/ViewModels/HomeViewModel.cs
+++ Models/ViewModels/HomeViewModel.cs
@@ -4,5 +4,6 @@ using System.Collections.Generic;
 namespace blog.Models{
     public class HomeViewModel{
         public List<Category> categories;
+        public List<Article> articles;
     }
 }
