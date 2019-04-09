9ffd8c5824bf58e27044f73d8006f70ce31b070f
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-09 21:58:05 +0300

    CategoryContext Inject

--- Startup.cs
+++ Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using blog.Implementers;
+using blog.Interfaces;
 using blog.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -37,6 +39,7 @@ namespace blog
                 options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
                 );
 
+            services.AddTransient<ICategory, CategoryContext>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
