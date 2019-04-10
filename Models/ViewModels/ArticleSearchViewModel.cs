3f585f46cdbf87b3da20f808ceaadab77b1fedc2
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 00:57:56 +0300

    view model for search page

---
+++ Models/ViewModels/ArticleSearchViewModel.cs
@@ -0,0 +1,9 @@
+
+using System.Collections.Generic;
+
+namespace blog.Models{
+    public class ArticleSearchViewModel{
+        public List<Category> categories;
+        public List<Article> articles;
+    }
+}
