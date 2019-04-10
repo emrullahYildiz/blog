f69f2152e664974e5a612e263edd5037905d9a4d
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 02:48:21 +0300

    Passed article content to view

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -1,12 +1,24 @@
 
+using blog.Interfaces;
+using blog.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blog.Controllers{
     public class ArticleController:Controller{
-
-        public IActionResult Index(){
-            
-            return View();
+        private readonly IArticle articleContext;
+        private readonly ICategory categoryContext;
+        public ArticleController(IArticle articleContext,ICategory categoryContext){
+            this.articleContext = articleContext;
+            this.categoryContext = categoryContext;
+        }
+        [HttpGet("/Article/Index/{articleId?}")]
+        public IActionResult Index(int articleId)
+        {
+            ArticleIndexViewModel articleViewModel = new ArticleIndexViewModel{
+                articleContent = articleContext.getArticleById(articleId),
+                categories = categoryContext.getCategories()
+            };
+            return View(articleViewModel);
         }
     }
 }
