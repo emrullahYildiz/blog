ce646cdc3d3095d047948ef6fccd724550677834
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 02:49:25 +0300

    ArticleIndexViewModel created

---
+++ Models/ViewModels/ArticleIndexViewModel.cs
@@ -0,0 +1,9 @@
+
+using System.Collections.Generic;
+
+namespace blog.Models{
+    public class ArticleIndexViewModel{
+        public Article articleContent;
+        public List<Category> categories;
+    }
+}
