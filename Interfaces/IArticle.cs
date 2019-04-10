6e087f88440bbff575204d4358bacb5440bd0e09
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 00:57:38 +0300

    search article function added

--- Interfaces/IArticle.cs
+++ Interfaces/IArticle.cs
@@ -6,9 +6,8 @@ namespace blog.Interfaces{
     public interface IArticle
     {
         List<Article> getArticles(int count);
-
         List<Article> getArticles(int page,int articleCount);
-
         int getArticleCount();
+        List<Article> searchArticle(string keywords);
     }
 }
