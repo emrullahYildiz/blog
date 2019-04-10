a14d82fa75d0c6b0eb736fbe0bae0a201f2eb5b2
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-10 23:15:33 +0300

    pagination component's model created

---
+++ Models/ViewModels/PaginationViewModel.cs
@@ -0,0 +1,10 @@
+
+namespace blog.Models{
+    public class PaginationViewModel{
+        public int currentPageNumber;
+        public int previousPageNumber;
+        public int nextPageNumber;
+        public int mostNextPageNumber;
+        public int mostPreviousPageNumber;
+    }
+}
