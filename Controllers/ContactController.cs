d52af1b431cabd53672ccd60d27cd08d75de399d
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 04:23:50 +0300

    Contact form method created

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,17 +1,23 @@
 
+using blog.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blog.Controllers{
     public class ContactController:Controller{
-        public IActionResult Index(){
-            
+        private readonly IContact contactContext;
+
+        public ContactController(){
+            this.contactContext = contactContext;
+        }
+        public IActionResult Index(IContact contactContex){
+
             return View();
         }
 
         [HttpPost]
         public IActionResult Index(string contactName,string contactMail,string contactMessage){
-
-            return View()
+            contactContext.AddConctactMessage(contactName,contactMail,contactMessage);
+            return View();
         }
     }
 }
