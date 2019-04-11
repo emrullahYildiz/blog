9365b39d9e8a41e44fbf2472d05de1ab377b4908
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 04:19:28 +0300

    contact model updated (contact date)

--- Models/DataBaseModels/Contact.cs
+++ Models/DataBaseModels/Contact.cs
@@ -1,9 +1,12 @@
+using System;
+
 namespace blog.Models{
     public class Contact{
         public int ContactID { get; set; }
         public string ContactFullName { get; set; }
         public string ContactMail { get; set; }
-        public int ContactMessage { get; set; }
+        public string ContactMessage { get; set; }
+        public DateTime ContactDate { get; set; }
         public bool ContactCheck { get; set; }
     }
 }
