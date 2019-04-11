bc344f8167f4c1b781d37d7f6e3bbfc6b537f076
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 04:29:03 +0300

    pass category data for layout article (Contact page shares article layout template)

--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -1,23 +1,31 @@
 
 using blog.Interfaces;
+using blog.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blog.Controllers{
     public class ContactController:Controller{
         private readonly IContact contactContext;
+        private readonly ICategory categoryContext;
 
-        public ContactController(){
-            this.contactContext = contactContext;
+        public ContactController(IContact contactContex,ICategory categoryContext){
+            this.contactContext = contactContex;
+            this.categoryContext = categoryContext;
         }
         public IActionResult Index(IContact contactContex){
-
-            return View();
+            ContactViewModel contactViewModel = new ContactViewModel{
+                categories = categoryContext.getCategories()
+            };
+            return View(contactViewModel);
         }
 
         [HttpPost]
         public IActionResult Index(string contactName,string contactMail,string contactMessage){
             contactContext.AddConctactMessage(contactName,contactMail,contactMessage);
-            return View();
+            ContactViewModel contactViewModel = new ContactViewModel{
+                categories = categoryContext.getCategories()
+            };
+            return View(contactViewModel);
         }
     }
 }
