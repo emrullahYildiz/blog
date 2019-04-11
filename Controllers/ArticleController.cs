28f7f4f2884e0571fbfdfaabca9c14f85aed19c4
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 03:40:46 +0300

    article comment post data pass to database

--- Controllers/ArticleController.cs
+++ Controllers/ArticleController.cs
@@ -7,9 +7,11 @@ namespace blog.Controllers{
     public class ArticleController:Controller{
         private readonly IArticle articleContext;
         private readonly ICategory categoryContext;
-        public ArticleController(IArticle articleContext,ICategory categoryContext){
+        private readonly IComment commentContext;
+        public ArticleController(IArticle articleContext,ICategory categoryContext,IComment commentContext){
             this.articleContext = articleContext;
             this.categoryContext = categoryContext;
+            this.commentContext = commentContext;
         }
         [HttpGet("/Article/Index/{articleId?}")]
         public IActionResult Index(int articleId)
@@ -19,6 +21,12 @@ namespace blog.Controllers{
                 categories = categoryContext.getCategories()
             };
             return View(articleViewModel);
+        }
+        /*Add comment form */
+        [HttpPost]
+        public IActionResult Index(string commentFullName,string commentMail,string commentContent,int articleId){
+            commentContext.AddComment(commentFullName,commentMail,commentContent,articleId);
+            return Redirect("/Article/Index/"+articleId);
         }
     }
 }
