ea6ac2127f3a445548e7f69c8a0070542e4fbdfe
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-11 03:12:00 +0300

    add mail column to comment table

---
+++ Migrations/20190411000943_CommentMail.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace blog.Migrations
+{
+    public partial class CommentMail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CommentMail",
+                table: "Comments",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CommentMail",
+                table: "Comments");
+        }
+    }
+}
