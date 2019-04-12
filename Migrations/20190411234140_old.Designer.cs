94081ee8e4cf654559d01ae383a09eab4c8bd9c0
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-12 15:54:19 +0300

    AdminUser table added

--- Migrations/20190411234140_old.Designer.cs
+++ Migrations/20190411234140_old.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using blog.Models;
 
 namespace blog.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190411234140_old")]
+    partial class old
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -30,21 +32,6 @@ namespace blog.Migrations
                     b.HasKey("AboutID");
 
                     b.ToTable("About");
-                });
-
-            modelBuilder.Entity("blog.Models.AdminUser", b =>
-                {
-                    b.Property<int>("AdminUserID")
-                        .ValueGeneratedOnAdd()
-                        .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
-
-                    b.Property<string>("UserName");
-
-                    b.Property<string>("UserPassword");
-
-                    b.HasKey("AdminUserID");
-
-                    b.ToTable("AdminUsers");
                 });
 
             modelBuilder.Entity("blog.Models.Article", b =>
---
+++ Migrations/20190412124556_adminUserTable.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace blog.Migrations
+{
+    public partial class adminUserTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AdminUsers",
+                columns: table => new
+                {
+                    AdminUserID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    UserName = table.Column<string>(nullable: true),
+                    UserPassword = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AdminUsers", x => x.AdminUserID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AdminUsers");
+        }
+    }
+}
