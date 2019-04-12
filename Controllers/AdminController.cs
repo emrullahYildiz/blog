4ae120f709cf09b6c9547560a0ea27fd7a5483d5
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-12 19:59:32 +0300

    login logout process added

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -1,5 +1,10 @@
 
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
 using blog.Interfaces;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace blog.Controllers{
@@ -10,6 +15,34 @@ namespace blog.Controllers{
         }
 
         public IActionResult Index(){
+            return View();
+        }
+        /*Login process run here */
+        [HttpPost]
+        public async Task<IActionResult> Index(string username,string password){
+            if(userContext.checkUser(username,password))//check user parameter
+            {
+                /*set User prop */
+                var claims = new List<Claim>{
+                    new Claim("user",username),
+                    new Claim("role","admin")
+                };
+                /*Sign in */
+                await HttpContext.SignInAsync(new ClaimsPrincipal(new ClaimsIdentity(claims, "Cookies", "user", "role")));
+                return Redirect("/Admin/Panel");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout(){
+            await HttpContext.SignOutAsync();
+            return Redirect("/Admin/Panel");
+        }
+
+        
+        [Authorize]
+        public IActionResult Panel(){
+
             return View();
         }
     }
