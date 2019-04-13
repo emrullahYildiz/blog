2c7196da4b34aec0aab7bc105cb7bbc2291c9077
emrullahYildiz <yildiz.emrullah@outlook.com>
2019-04-13 08:25:21 +0300

    Add category function added

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -12,14 +12,17 @@ namespace blog.Controllers{
     public class AdminController:Controller{
         private readonly IUser userContext;
         private readonly IComment commentContext;
-        public AdminController(IUser userContext,IComment commentContext){
+        private readonly ICategory categoryContext;
+        public AdminController(IUser userContext,IComment commentContext,ICategory categoryContext){
             this.userContext = userContext;
             this.commentContext = commentContext;
+            this.categoryContext = categoryContext;
         }
 
         public IActionResult Index(){
             return View();
         }
+        
         /*Login process run here */
         [HttpPost]
         public async Task<IActionResult> Index(string username,string password){
@@ -47,8 +50,8 @@ namespace blog.Controllers{
 
             return View();
         }
+        
         /*Comment process start */
-
         [Authorize]
         public IActionResult Comments(){
             //passed data is last 10 comment
@@ -60,8 +63,8 @@ namespace blog.Controllers{
             };
             return View(commentsViewModel);
         }
-
         //Comment search by article head
+        
         [Authorize]
         [HttpPost]
         public IActionResult Comments(string articleHead){
@@ -71,14 +74,42 @@ namespace blog.Controllers{
             };
             return View(commentsViewModel);
         }
-
+        
         [Authorize]
         [Route("/Admin/DeleteComment/{id?}")]
         public IActionResult DeleteComment(int id){
             commentContext.DeleteCommentById(id);
             return Redirect("/Admin/Comments");
         }
-
         /*Comment process end */
+
+        /*Category process start */
+        [Authorize]
+        public IActionResult Category(){
+            CategoryViewModel categoryViewModel = new CategoryViewModel{
+                categories = categoryContext.getCategories()
+            };
+            return View(categoryViewModel);
+        }
+
+        /*Add category process runs here */
+        [Authorize]
+        [HttpPost]
+        public IActionResult Category(string categoryName){
+            //Category add
+            categoryContext.AddCategory(categoryName);
+            //category info to view
+            CategoryViewModel categoryViewModel = new CategoryViewModel{
+                categories = categoryContext.getCategories()
+            };
+            return View(categoryViewModel);
+        }
+        [Authorize]
+        [Route("/Admin/Category/{categoryId}")]
+        public IActionResult CategoryDelete(int categoryId){
+            
+            return Redirect("/Admin/Category");
+        }
+        /*Category process  end*/
     }
 }
